b1df52f7d1cf5096ddf6fe995b9181b9995a5faa
Hyungu Kahng <drlego9@gmail.com>
2019-11-27 12:09:10 +0900

    add description with echo

--- distillation-kor/1.5_run_merge_dumps.sh
+++ distillation-kor/1.5_run_merge_dumps.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Merging multiple dump files."
+echo "Add or delete more files to the `--in_files` argument at will."
+
 python distillation-kor/scripts/merge_dumps.py \
     --dump_root '.data/' \
     --in_files 'dump_wiki-kor.txt' 'dump_aihub.txt' 'dump_ucorpus.txt' 'dump_news-kor.txt' \
